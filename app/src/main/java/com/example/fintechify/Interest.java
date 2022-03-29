f7d385d24242861379008a1b1d89420ba51ff445
Ricky Tran <rickie.tran7@gmail.com>
2022-03-28 22:09:10 -0400

    Removed the Salary After Tax and Expenses Sheet Activities. The Currency Conversion and Finance News Activity have been put in lieu. The Currency Conversion Activity works with API but its limited to the Spinner Widget options.

--- app/src/main/java/com/example/fintechify/Interest.java
+++ app/src/main/java/com/example/fintechify/Interest.java
@@ -8,11 +8,12 @@ import android.view.View;
 import android.widget.Button;
 
 public class Interest extends AppCompatActivity {
-    private Button back;
+    private Button back, simple, compound;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_interest);
+
         back = (Button) findViewById(R.id.back);
         back.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -20,10 +21,34 @@ public class Interest extends AppCompatActivity {
                 goBack();
             }
         });
+
+        simple = (Button) findViewById(R.id.calculate);
+        simple.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goSimple();
+            }
+        });
+
+        compound = (Button) findViewById(R.id.compound);
+        compound.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goCompound();
+            }
+        });
     }
 
     public void goBack(){
         Intent intent = new Intent(this, HomePage.class);
+        startActivity(intent);
+    }
+    public void goSimple(){
+        Intent intent = new Intent(this, simpleInterest.class);
+        startActivity(intent);
+    }
+    public void goCompound(){
+        Intent intent = new Intent(this, compoundInterest.class);
         startActivity(intent);
     }
 }
